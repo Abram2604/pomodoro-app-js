e54f3bcb686337ccf249a23d6847f834f10a87ea
Abram <abdulramalah26@gmail.com>
2025-10-27 21:24:43 +0700

    penambahan untuk deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  output: "export",
+  basePath: "/pomodoro-app-js",
+  assetPrefix: "/pomodoro-app-js/",
 };
 
 export default nextConfig;
